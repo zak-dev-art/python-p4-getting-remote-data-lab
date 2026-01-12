d139c2724e7fb66d97c602719ee5792026170670
zak-dev-art <zachndungu861@gmail.com>
2026-01-12 15:37:54 +0300

    modified changes

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
